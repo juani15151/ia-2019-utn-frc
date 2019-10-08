0c9b8231e21e041d4cc5f9627db4b3c9adc08973
juani15151 <15637690+juani15151@users.noreply.github.com>
2019-10-07 22:02:14 -0300

    Simplificacion del codigo

--- main.py
+++ main.py
@@ -16,25 +16,26 @@ def main():
     with open('Y_train.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for line in csv_reader:
-            salida_esperada.append(float(line[0]))
+            salida_esperada.append(int(line[0]))
 
 
     assert len(entradas) == len(salida_esperada)
+
+    colors = ['r', 'b']
 
     cantidad_caracteristicas = len(entradas[0])
     for x1 in range(0, cantidad_caracteristicas):
         for x2 in range(x1, cantidad_caracteristicas):
             for i in range(0, len(entradas)):
                 entrada_actual = entradas[i]
-                if salida_esperada[i] == 0:
-                    plt.scatter(entrada_actual[x1], entrada_actual[x2], c='r')
-                else:
-                    plt.scatter(entrada_actual[x1], entrada_actual[x2], c='b')
+                plt.scatter(entrada_actual[x1], entrada_actual[x2], c=colors[salida_esperada[i]])
             # plt.plot()  # Muestra el grafico
             plt.savefig("datos/x" + str(x1) + "-" + "x" + str(x2) + ".png")
             plt.clf()  # Limpia el grafico
 
 
+    # Informacion obtenida:
+    # X4 * Xi No aporta informacion
 
 
 
